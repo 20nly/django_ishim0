033c0cfec10b85ae4e4a64dabf4d154566bd3d54
shohrat <fgt0191@gmail.com>
2022-03-13 00:00:11 +0500

    3 before migration

--- board/views.py
+++ board/views.py
@@ -1,15 +1,60 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+resumeList = [
+    {
+    'id':'1',
+    'title':'водитель',
+    'description':'опыт вождения 3 года'
+    },
+    {
+    'id':'2',
+    'title':'бухгалтер',
+    'description':'окончил учебу РУДН'
+    },
+    {
+    'id':'3',
+    'title':'парикмахер',
+    'description':'Карэ кабярэ'
+    },
+]
 
-
+vacancyList = [
+    {
+    'id':'4',
+    'title':'Требуется повар',
+    'description':'Опыт кондитер'
+    },
+    {
+    'id':'5',
+    'title':'Требуется Няня',
+    'description':'фыворол фыволрфывапдофыдв'
+    },
+    {
+    'id':'6',
+    'title':'Требуется охранник',
+    'description':'фывфы дплдвфырпалырд'
+    },
+]
 
 
 def board(request):
-    return render(request, 'board/board.html')
+    page = 'board'
+    context = {'page':page, 'board_resume':resumeList,'board_vacancy':vacancyList}
+    return render(request, 'board/board.html', context)
+    
 
 def resume(request, pk):
-    return render(request, 'board/resume.html')
+    resumeObj = None
+    for i in resumeList:
+        if i['id'] == pk:
+            resumeObj = i
+    return render(request, 'board/resume.html',{'resume':resumeObj})
+
 
 def vacancy(request, pk):
-    return render(request, 'board/vacancy.html')
+    vacancyObj = None
+    for i in vacancyList:
+        if i['id'] == pk:
+            vacancyObj = i
+    return render(request, 'board/vacancy.html', {'vacancy':vacancyObj})
